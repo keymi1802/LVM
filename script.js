82161e522aa791b7be9d436db6e14643e1db9902
keymi1802 <130862902+keymi1802@users.noreply.github.com>
2026-04-09 23:20:28 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,116 @@
+window.addEventListener("scroll", function(){
+const navbar = document.querySelector(".top-bar");
+if(window.scrollY > 50){
+navbar.classList.add("scrolled");
+}else{
+navbar.classList.remove("scrolled");
+}
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+
+const slider = document.getElementById("slider");
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+
+next.addEventListener("click", function(){
+slider.scrollBy({ left: 350, behavior: "smooth" });
+});
+
+prev.addEventListener("click", function(){
+slider.scrollBy({ left: -350, behavior: "smooth" });
+});
+
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+
+const track = document.querySelector(".carousel-track");
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+const carousel = document.querySelector(".carousel");
+const items = document.querySelectorAll(".carousel-item");
+
+// FLECHAS
+next.addEventListener("click", () => {
+track.scrollBy({ left: 200, behavior: "smooth" });
+});
+
+prev.addEventListener("click", () => {
+track.scrollBy({ left: -200, behavior: "smooth" });
+});
+
+// APARECER CON SCROLL
+window.addEventListener("scroll", () => {
+
+if(window.scrollY > 50){
+carousel.classList.add("show");
+}else{
+carousel.classList.remove("show");
+}
+
+// ACTIVO
+items.forEach(item => {
+
+const section = document.getElementById(item.dataset.target);
+
+if(section){
+const rect = section.getBoundingClientRect();
+
+if(rect.top < 200 && rect.bottom > 200){
+item.classList.add("active");
+}else{
+item.classList.remove("active");
+}
+}
+
+});
+
+});
+
+// CLICK → IR A SECCIÓN
+items.forEach(item => {
+
+item.addEventListener("click", () => {
+
+const section = document.getElementById(item.dataset.target);
+
+if(section){
+section.scrollIntoView({
+behavior:"smooth"
+});
+}
+
+});
+
+});
+
+});
+
+const track = document.querySelector('.carousel-track');
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+track.addEventListener('mousedown', (e) => {
+isDown = true;
+startX = e.pageX - track.offsetLeft;
+scrollLeft = track.scrollLeft;
+});
+
+track.addEventListener('mouseleave', () => {
+isDown = false;
+});
+
+track.addEventListener('mouseup', () => {
+isDown = false;
+});
+
+track.addEventListener('mousemove', (e) => {
+if(!isDown) return;
+e.preventDefault();
+const x = e.pageX - track.offsetLeft;
+const walk = (x - startX) * 2;
+track.scrollLeft = scrollLeft - walk;
+});
